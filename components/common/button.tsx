6c85d32c8b3dcf3825f79acc2d75ec3e7b9b2132
Limitlex Technologies <kobbygazi@gmail.com>
2025-11-02 23:05:24 +0000

    Updated

--- components/common/button.tsx
+++ components/common/button.tsx
@@ -3,11 +3,13 @@ import { ButtonProps } from "../../interfaces";
 
 const Button: React.FC<ButtonProps> = ({ label, onClick, style }) => {
   return (
+//this is a nice button component
     <button
       onClick={onClick}
       className={`px-8 py-3 font-medium text-[20px]/[1.5] rounded-[60px] ${style}`}
     >
       {label}
+
     </button>
   );
 };
--- components/common/card.tsx
+++ components/common/card.tsx
@@ -6,6 +6,7 @@ const Card: React.FC<CardProps> = ({ title, description }) => {
     <div className="border p-4 rounded shadow-sm">
       <h2 className="text-xl font-semibold">{title}</h2>
       <p className="text-sm text-gray-600">{description}</p>
+      
     </div>
   );
 };
